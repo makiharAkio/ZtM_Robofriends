210a3aa3b42e8be3b94fb89a70e592e0a16e1d22
Makihara Akio <akio.makihara@gmail.com>
2023-03-10 22:38:20 +0900

    class 237

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from './SearchBox';
+import './App.css';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        };
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json() )
+        .then(users => this.setState({ robots: users }));
+    }
+    
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render () {
+        const filterRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange = { this.onSearchChange } />
+                <CardList robots = { filterRobots } />
+            </div>
+        );
+    }
+}
+
+export default App;
